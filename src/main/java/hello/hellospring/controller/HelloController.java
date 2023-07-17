8d10b13e1e02863304dc38ab5a57eb774948b6ad
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2023-07-18 00:18:31 +0900

    7/16

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,14 +3,49 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 
 public class HelloController {
     @GetMapping("hello")
     public String hello(Model model){
-        model.addAttribute("data", "Spring!!");
+        model.addAttribute("data", "hello!!");
         return "hello";
 
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name")String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody // html body부에 직접 넣어주는 것
+    public String helloString(@RequestParam("name") String name){
+        return "hello " + name; // "hello spring"
+    }
+
+    @GetMapping("hello-api") // json 방식
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello{
+        private String name;
+
+        // getter and setter : alt + insert
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
