0860a93a78c90f22acdac85dbb6d0633a290bbd7
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2023-07-20 02:31:00 +0900

    7/16

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -9,7 +9,11 @@ import java.util.Optional;
 
 public class MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     /**
      * 회원 가입
@@ -29,7 +33,7 @@ public class MemberService {
     private void validateDuplicateMember(Member member) {
         memberRepository.findByName(member.getName())
                 .ifPresent(m -> {
-                    throw new IllegalStateException("이미 존재하는 회원입니다/"); // IllegalStateException : null값이 아닌 값이 오면 동작하는 것
+                    throw new IllegalStateException("이미 존재하는 회원입니다."); // IllegalStateException : null값이 아닌 값이 오면 동작하는 것
                 });
     }
 
